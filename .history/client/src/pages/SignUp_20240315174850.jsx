0d20df76800f60b23d286297c8a33b8d9aca07aa
Artur <artgouveiag@gmail.com>
2024-03-15 17:58:19 +0100

    completed signup page UI

--- .history/client/src/pages/SignUp_20240315174850.jsx
+++ .history/client/src/pages/SignUp_20240315174850.jsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom';
+
 
 function SignUp() {
   return (
@@ -10,38 +10,38 @@ function SignUp() {
           placeholder='username'
           className='border p-3 rounded-lg'
           id='username'
-          
+          onChange={handleChange}
         />
         <input
           type='email'
           placeholder='email'
           className='border p-3 rounded-lg'
           id='email'
-          
+          onChange={handleChange}
         />
         <input
           type='password'
           placeholder='password'
           className='border p-3 rounded-lg'
           id='password'
-          
+          onChange={handleChange}
         />
 
         <button
           // disabled={loading}
           className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
         >
-          Sign Up
+          {/* {loading ? 'Loading...' : 'Sign Up'} */}
         </button>
         {/* <OAuth/> */}
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Have an account?</p>
-        <Link to={'/sign-in'}>
+        <Li to={'/sign-in'}>
           <span className='text-blue-700'>Sign in</span>
-        </Link>
+        </Li>
       </div>
-      {/* {error && <p className='text-red-500 mt-5'>{error}</p>} */}
+      {error && <p className='text-red-500 mt-5'>{error}</p>}
     </div>
   );
 }
--- .history/client/src/pages/SignUp_20240315175532.jsx
+++ .history/client/src/pages/SignUp_20240315175532.jsx
@@ -31,7 +31,7 @@ function SignUp() {
           // disabled={loading}
           className='bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80'
         >
-          Sign Up
+          
         </button>
         {/* <OAuth/> */}
       </form>
