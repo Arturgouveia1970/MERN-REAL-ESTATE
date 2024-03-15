173aa46edf92fb29febf445c4c4f0eaed4e722e8
Artur <artgouveiag@gmail.com>
2024-03-15 18:52:42 +0100

    created signin api route

--- .history/api/routes/auth.route_20240315184811.js
+++ .history/api/routes/auth.route_20240315184811.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { signin, signup } from '../controllers/auth.controller.js';
+import { signup } from '../controllers/auth.controller.js';
 
 const router = express.Router();
 
